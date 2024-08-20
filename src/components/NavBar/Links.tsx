4aec58585cec7e209b893ec78152f72abaf10ba8
abdurrehman0206 <abdurrehmansaleem5@gmail.com>
2024-08-21 01:33:57 +0500

    Fixed Nav

--- src/components/NavBar/Links.tsx
+++ src/components/NavBar/Links.tsx
@@ -4,6 +4,6 @@ const Links = () => {
   const linkElement = LinksArray.map((link) => {
     return <NavLink key={link.id} {...link} />;
   });
-  return <div>{linkElement} </div>;
+  return <div className="space-y-2">{linkElement} </div>;
 };
 export default Links;
