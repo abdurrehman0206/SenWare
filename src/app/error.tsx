a6872f6775d41065bdeb5040930325a9a4ea9a73
Mairaj001 <mairajahmed2k21@gmail.com>
2024-08-22 00:44:43 +0500

    commit

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,5 +1,11 @@
 "use client";
+
 const Error = () => {
-  return <div></div>;
+  return (
+    <div className="bg-red-200 border border-red-600 text-red-300 px-4 py-3 rounded relative" role="alert">
+      <strong className="font-bold">Error!</strong>
+      <span className="block sm:inline"> An error occurred. Please try again later.</span>
+    </div>
+  );
 };
 export default Error;
