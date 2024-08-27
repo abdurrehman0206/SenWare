e22ba52fbbd6ded7bbf8a3da96d05656a1084d6c
abdurrehman0206 <abdurrehmansaleem5@gmail.com>
2024-08-27 07:55:52 +0500

    Stat Count Update

--- src/components/Items/ItemsStatBar.tsx
+++ src/components/Items/ItemsStatBar.tsx
@@ -6,32 +6,29 @@ import { StorageIcon } from "@/components/Icons/Storage";
 import StatCard from "@/components/Stats/StatCard";
 import Bounded from "@/components/Utils/Bounded";
 import { useItemsContext } from "@/hooks/useItemsContext";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import { InventoryIcon } from "../Icons/Inventory";
 
 const ItemsStatBar = () => {
-  const [categoryCount, setCategoryCount] = useState<number | undefined>(0);
-  const [revenue, setRevenue] = useState<number | undefined>(0);
-  const [quantity, setQuantity] = useState<number | undefined>(0);
+
   const { items } = useItemsContext();
-  useEffect(() => {
-    const fetchStats = async () => {
-      const result = await getCategoryCount();
-      if (result.count) {
-        setCategoryCount(result.count);
-      } else {
-        console.log(result.error);
-      }
-      let totalRev = 0;
-      let totalQuan = 0;
-      items.forEach((item) => {
-        totalQuan += item.quantity;
-        totalRev += item.quantity * item.price;
-      });
-      setRevenue(totalRev);
-      setQuantity(totalQuan);
+  const statCount = useMemo(() => {
+    let totalRevenue = 0;
+    let totalQuantity = 0;
+    const categories = new Set<string>();
+
+    items.forEach(({ quantity, price, categoryName }) => {
+      totalQuantity += quantity;
+      totalRevenue += quantity * price;
+      categories.add(categoryName);
+    });
+
+    return {
+      totalItems: items.length,
+      totalQuantity,
+      totalRevenue,
+      totalCategories: categories.size,
     };
-    fetchStats();
   }, [items]);
   return (
     <Bounded>
@@ -42,7 +39,7 @@ const ItemsStatBar = () => {
             className: "font-semibold text-blue-400",
           }}
           statIcon={<StorageIcon className={"w-6 h-6 stroke-blue-400"} />}
-          statCount={items.length || 0}
+          statCount={statCount.totalItems || 0}
         />
         <StatCard
           statHeader={{
@@ -50,7 +47,7 @@ const ItemsStatBar = () => {
             className: "font-semibold text-purple-400",
           }}
           statIcon={<InventoryIcon className={"w-6 h-6 stroke-purple-400"} />}
-          statCount={quantity || 0}
+          statCount={statCount.totalQuantity || 0}
         />
         <StatCard
           statHeader={{
@@ -58,7 +55,7 @@ const ItemsStatBar = () => {
             className: "font-semibold text-orange-400",
           }}
           statIcon={<CategoryIcon className={"w-6 h-6 stroke-orange-400"} />}
-          statCount={categoryCount || 0}
+          statCount={statCount.totalCategories || 0}
         />
         <StatCard
           statHeader={{
@@ -66,7 +63,7 @@ const ItemsStatBar = () => {
             className: "font-semibold text-emerald-400",
           }}
           statIcon={<MoneyBillIcon className={"w-6 h-6 stroke-emerald-400"} />}
-          statCount={revenue || 0}
+          statCount={statCount.totalRevenue || 0}
         />
       </div>
     </Bounded>
