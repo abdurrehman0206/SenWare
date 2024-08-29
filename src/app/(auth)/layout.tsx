c3e0af434137154628f426b8fee7719815963108
abdurrehman0206 <abdurrehmansaleem5@gmail.com>
2024-08-30 04:50:29 +0500

    Added Logo On Login

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,8 +1,16 @@
+import Image from "next/image";
 import { TabsBar } from "./TabBar";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-screen bg-teal-200 flex flex-col justify-center items-center gap-2">
+      <Image
+        src={"/WhiteLogo.png"}
+        alt={"Logo"}
+        width={120}
+        height={120}
+        className="-mt-10 mb-20"
+      />
       <TabsBar />
       {children}
     </div>
